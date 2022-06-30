a8ec7abd990a69ddba41b906ff9e2d2ce50688be
Dmitriy Homo <69244728+dimensenss@users.noreply.github.com>
2022-06-30 17:49:52 +0300

    ok

--- Работа со строками/Вывести расширение файла/Вывести_расширение_файла.py
+++ Работа со строками/Вывести расширение файла/Вывести_расширение_файла.py
@@ -1,12 +1,9 @@
 #-*- coding:1251 -*-
-#Дано имя файла. Необходимо вывести его расширение.
 def extention(name):
     return name.split('.')[1]
 
 #def extention(name):
-#    # Находим индекс точки
 #    point_pos = filename.find('.')
-#    # Возвращаем срез начиная с позиции после точки и до конца имени файла
 #    return filename[point_pos + 1:]
 
 FILE = 'folder1/folder2/file.ext'
