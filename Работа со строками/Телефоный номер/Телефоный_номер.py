8b0959eb8f9e5cac0dc3b4d0bb2f7a56e2736415
dima <dimensenss@gmail.com>
2022-07-13 19:45:01 +0300

    ok

--- Работа со строками/Телефоный номер/Телефоный_номер.py
+++ Работа со строками/Телефоный номер/Телефоный_номер.py
@@ -6,5 +6,4 @@ def create_phone_number(n):
 
 
 print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
-print(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-
+print(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 2]))
