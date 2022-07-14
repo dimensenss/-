036a924d86bd42c97e9e89036c02061da2bc90bb
dima <dimensenss@gmail.com>
2022-07-14 19:58:06 +0300

    ok

--- Работа со строками/Сложите цифры целого числа/Сложите_цифры_целого_числа.py
+++ Работа со строками/Сложите цифры целого числа/Сложите_цифры_целого_числа.py
@@ -1,14 +1,14 @@
-
 def num_sum(num):
     lst = list(str(num))
     s = 0
-    for i in lst: s += int(i)
+    for i in lst:
+        s += int(i)
     return s
+
 
 def num_suma(num):
     lst = [int(item) for item in str(num)]
     return sum(lst)
-
 
 
 print(num_sum(32345))
